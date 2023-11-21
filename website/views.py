abcce0463e5e1bfe1b15c4ad60854492a927c035
Mark <71256103+Justtforthat@users.noreply.github.com>
2023-11-21 21:39:56 +0600

    replaced and created user page

--- website/views.py
+++ website/views.py
@@ -12,6 +12,31 @@ def main():
 def prod():
     return render_template("prod.html", user=current_user)
 
+@views.route('/pc')
+def pc():
+    return render_template("pc.html", user=current_user)
+
+@views.route('/pc2')
+def pc2():
+    return render_template("pc2.html", user=current_user)
+
+@views.route('/pc3')
+def pc3():
+    return render_template("pc3.html", user=current_user)
+
+@views.route('/pc4')
+def pc4():
+    return render_template("pc4.html", user=current_user)
+
+@views.route('/pc5')
+def pc5():
+    return render_template("pc5.html", user=current_user)
+
+@views.route('/pc6')
+def pc6():
+    return render_template("pc6.html", user=current_user)
+
+
 @views.route('/add-product', methods=['POST'])
 @login_required
 def add_product():
@@ -32,26 +57,30 @@ def add_product():
 @views.route('/delete-product/<int:product_id>', methods=['POST'])
 @login_required
 def delete_product(product_id):
-    product = Product.query.get_or_404(product_id)
-    if product.user_id != current_user.id:
+    product = Product.query.get(product_id)
+    if not product:
+        flash('Product not found.', category='error')
+    elif product.user_id != current_user.id:
         flash('You do not have permission to delete this product.', category='error')
-        return redirect(url_for('views.profile'))
-    db.session.delete(product)
-    db.session.commit()
-    flash('Product has been deleted!', category='success')
+    else:
+        db.session.delete(product)
+        db.session.commit()
+        flash('Product has been deleted!', category='success')
     return redirect(url_for('views.profile'))
+
 
 @views.route('/add_computer/<int:computer_id>')
 @login_required
 def add_computer(computer_id):
     computer = Computer.query.get(computer_id)
-    if computer and computer.user_id is None:  # Check if computer is not already added by another user
+    if computer and computer.user_id is None:
         computer.user_id = current_user.id
         db.session.commit()
-        flash('Computer added to your list', 'success')
+        flash('Computer added to your list', category='success')
     else:
-        flash('Computer not found or already added by someone else', 'error')
-    return redirect(url_for('views.pc'))
+        flash('Computer not found or already added by someone else', category='error')
+    return redirect(url_for('views.my_computers'))
+
 
 @views.route('/my_computers')
 @login_required
@@ -75,6 +104,10 @@ def computer(computer_id):
 
     return render_template('computer_details.html', computer=computer, comments=comments)
 
+@views.route('/user_page')
+@login_required
+def user_page():
+    return render_template('user_page.html', user=current_user)
 
 @views.route('/profile')
 @login_required
